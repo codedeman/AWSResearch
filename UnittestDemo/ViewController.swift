ee6ca40ee53607526d9eecf65e28c53c9f14bb6d
KienPham01 <kienluproductions@gmail.com>
2020-02-10 23:36:50 +0700

    first commit

---
+++ UnittestDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UnittestDemo
+//
+//  Created by Apple on 2/10/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+import AWSMobileAnalytics
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
